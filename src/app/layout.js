99679c749300e9736b1680f37f02d6a85168758b
viralhysteria <1983431+viralhysteria@users.noreply.github.com>
2023-10-19 20:30:41 -0500

    build: npx create-next-app@latest

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
